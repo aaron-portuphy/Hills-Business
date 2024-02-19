c22f793010000c77be6f69249720742ab026f8f0
aaron-portuphy <72166690+aaron-portuphy@users.noreply.github.com>
2024-02-19 15:49:58 +0000

    Components upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import Home from './components/Home/Home';
+import About from './components/About/About';
+
+
+function App() {
+  return (
+    <div className="App">
+      {/* Components */}
+      <Navbar />
+      <Home />
+      <About />
+    </div>
+  );
+}
+
+export default App;
